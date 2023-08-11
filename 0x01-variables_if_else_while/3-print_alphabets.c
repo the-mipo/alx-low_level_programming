d8ee4516dbfa4c41cc8fe365b26240a6d6930dd9
the-mipo <bukolajesutofunmi13@gmail.com>
2023-08-11 08:41:34 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Return: Always 0
+ * Result: always 0
  */
 int main(void)
 {
 	int n = 97;
+	int m = 65;
 
 	while (n <= 122)
 	{
 		putchar(n);
 		n++;
+	}
+	while (m <= 90)
+	{
+		putchar(m);
+		m++;
 	}
 	putchar('\n');
 	return (0);
