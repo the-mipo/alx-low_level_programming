4beeb7b050b765c23f0b9b309db651e6b02158fc
the-mipo <bukolajesutofunmi13@gmail.com>
2023-08-15 08:15:44 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,6 +17,9 @@ void print_times_table(int n)
 				k = j * i;
 				if (j == 0)
 				{
+					_putchar(k + '0');
+				} else if (k < 10 && j != 0)
+				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
